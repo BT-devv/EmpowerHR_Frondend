f70d98b3a76f5373a9c8f0d363c609498b87ddbd
oniqy <vyle.111003@gmail.com>
2024-12-11 18:25:34 +0700

    test 06

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <p>Bùi Trung Tuấn</p>
-      <p>Lê Thái Vỹ</p>
+      <h2>Lê Thái Vỹ</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

06b35be0d0661883344c08efccf1a30e42d8767b
BT-devv <buitrungtuan2003@gmail.com>
2024-12-11 18:21:42 +0700

    Test server 5

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <p>Bùi Trung Tuấn</p>
+      <p>BuiTrungTuan-2174801030024</p>
       <p>Lê Thái Vỹ</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>

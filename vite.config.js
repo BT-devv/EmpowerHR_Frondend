f8d776e3130514ac55bc78bbc1a4845239e3ef38
BT-devv <buitrungtuan2003@gmail.com>
2025-01-04 11:44:38 +0700

    vit config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
-import path from "path";
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    assetsInclude: ["**/*.ttf"], // Bao gồm font file trong build
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"), // Đảm bảo đúng alias nếu có
-    },
-  },
+  plugins: [react()],
 });

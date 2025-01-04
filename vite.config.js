633888286a8d66024c85f52966ab09d1a2fe29f9
BT-devv <buitrungtuan2003@gmail.com>
2025-01-04 11:39:35 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    assetsInclude: ["**/*.ttf"], // Bao gồm font file trong build
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Đảm bảo đúng alias nếu có
+    },
+  },
+});
